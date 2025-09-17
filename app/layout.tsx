c0263377aec696b737fc680c9401a66533614069
edunftmx <laloguajardo57@gmail.com>
2025-09-16 19:11:18 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,26 @@
 "use client";
 
-import "./globals.css";
+import "./globals.css"; // Asegúrate de que este archivo exista
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { createChain } from "thirdweb";
 
-const RONIN_CHAIN_ID = 2020;
+const RONIN = createChain({
+  id: 2020, // Chain ID de Ronin
+  name: "Ronin",
+  nativeCurrency: {
+    name: "Ronin",
+    symbol: "RON",
+    decimals: 18,
+  },
+  rpc: ["https://api.roninchain.com/rpc"],
+  blockExplorers: [
+    {
+      name: "Ronin Explorer",
+      url: "https://app.roninchain.com",
+    },
+  ],
+  testnet: false,
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ThirdwebProvider activeChain={RONIN_CHAIN_ID}>
+        <ThirdwebProvider activeChain={RONIN}>
           {children}
         </ThirdwebProvider>
       </body>
